b85a11e2e7ca15a58bfc6e86d5d1929c54947e27
Joshua T Kalis <kalisjoshua@gmail.com>
2012-09-11 12:22:41 -0400

    adding first draft of code and example in readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# Sherpa
+
+Watch files in a directory for changes/updates.
+
+I know that this might not be the best way to do this but it seems to work for right now so I am putting it out there. Simply create a file to execte in the terminal (example below) and point it at the directory you would like to watch. When a file is changed the callback you pass in will execute and you can do whatever you want with the change.
+
+I have started using this in conjunction with Grunt.js so I don't have to grunt with each change to my files while developing.
+
+## Example file
+
+    #!/usr/bin/env node
+    /*jshint strict:false*/
+    /*globals require*/
+    var exec = require('child_process').exec
+      , sherpa = require("sherpa")
+
+    function notify (filename, command) {
+      puts(null, "\nChange made to: " + filename);
+
+      command && exec(command, puts);
+    }
+
+    sherpa("src", 300, function (filename, curr, prev) {
+      if (+curr.mtime !== +prev.mtime) {
+        switch (/.*\.(.*)$/.exec(filename)[1]) {
+          case "js":
+            notify(filename, "grunt js");
+            break;
+          case "less":
+            notify(filename, "grunt less");
+            break;
+          default:
+            notify(filename + " but no action was taken");
+            break;
+        }
+      }
+    });
---
+++ sherpa.js
@@ -0,0 +1,62 @@
+/*jshint laxcomma:true strict:false*/
+/*globals require*/
+var fs   = require("fs")
+  , path = require("path")
+  , util = require('util');
+
+function puts(error, stdout/*, stderr*/) {
+  util.puts(stdout);
+}
+
+function walk (start, callback) {
+  fs.lstat(start, function (err, stat) {
+    if (err) { return callback(err); }
+    if (stat.isDirectory()) {
+
+      fs.readdir(start, function (err, files) {
+        var coll = files.reduce(function (acc, i) {
+          var abspath = path.join(start, i);
+
+          if (fs.statSync(abspath).isDirectory()) {
+            walk(abspath, callback);
+            acc.dirs.push(abspath);
+          } else {
+            acc.names.push(abspath);
+          }
+
+          return acc;
+        }, {"names": [], "dirs": []});
+
+        return callback(null, start, coll.names);
+      });
+    } else {
+      return callback(new Error("path: " + start + " is not a directory"));
+    }
+  });
+}
+
+function watch (root, interval, callback) {
+  if (arguments.length < 2) {
+    throw("Invalid arguments");
+  }
+
+  if (typeof callback === "undefined" && typeof interval === "function") {
+    callback = interval;
+    interval = undefined;
+  }
+
+  var opts = {
+          persistent: true
+        , interval: interval || 5007
+      };
+
+  puts(null, "Watching " + root + " every " + opts.interval / 1000 + " seconds for changes...");
+
+  walk(root, function (err, dir, files) {
+    files
+      .forEach(function (filename) {
+        fs.watchFile(filename, opts, callback.bind(null, filename));
+      });
+  });
+
+}
